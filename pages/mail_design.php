2e8ca318d2ee88bc8fc9fc5fd5a5bf090ec9f125
Emmanuel Mamani <emanuelriot@hotmail.com>
2024-11-21 19:04:40 -0300

    cambios

--- pages/mail_design.php
+++ pages/mail_design.php
@@ -44,55 +44,55 @@
         <td align="center" style="padding:0;">
           <table role="presentation" style="width:94%;max-width:600px; min-width:400px;border:none;border-spacing:0;text-align:left;font-family:Arial,sans-serif;font-size:16px;line-height:22px;color:#363636;">
             <tr style="background-color: white;">
-                <td style="padding:0;font-size:24px;line-height:28px;font-weight:bold;">
-                    <a href="https://max.zixfox.com.ar" style="text-decoration:none;">
-                        <img src="https://max.zixfox.com.ar/assets/images/mail.png" width="600" alt=""
-                            style="width:100%;height:auto;display:block;border:none;text-decoration:none;">
-                    </a>
-                </td>
+              <td style="padding:0;font-size:24px;line-height:28px;font-weight:bold;">
+                <a href="https://max.zixfox.com.ar" style="text-decoration:none;">
+                  <img src="https://max.zixfox.com.ar/assets/images/mail.png" width="600" alt=""
+                        style="width:100%;height:auto;display:block;border:none;text-decoration:none;">
+                </a>
+              </td>
             </tr>
             <tr>
-                <td style="padding:30px 30px 10px 30px;background-color:#ffffff;">
-                    <p style="margin:0; font-size: 25px; line-height: 30px; font-weight: bold;">
-                        Hola
-                        <?=$name_encode?>
-                    </p>
-                </td>
+              <td style="padding:30px 30px 10px 30px;background-color:#ffffff;">
+                <p style="margin:0; font-size: 25px; line-height: 30px; font-weight: bold;">
+                  Hola
+                  <?=$name_encode?>
+                </p>
+              </td>
             </tr>
             <tr>
-                <td style="padding:10px 30px;background-color:#ffffff;">
-                    <p style="margin:0;">
-                      Gracias por contactarnos, ahora ya tenemos toda la información que nos proporcionaste, 
-                      evaluaremos tu caso y nos pondremos en contacto contigo lo mas pronto posible
-                    </p>
-                </td>
+              <td style="padding:10px 30px;background-color:#ffffff;">
+                <p style="margin:0;">
+                  Gracias por contactarnos, ahora ya tenemos toda la información que nos proporcionaste, 
+                  evaluaremos tu caso y nos pondremos en contacto contigo lo mas pronto posible
+                </p>
+              </td>
             </tr>
             <tr>
-                <td style="padding:10px 30px;background-color:#ffffff;">
-                    <p style="margin:0; font-size: 13px; line-height: 20px;">
-                      Nos escribiste esto: <?=$args['message']?>
-                    </p>
-                </td>
+              <td style="padding:10px 30px;background-color:#ffffff;">
+                <p style="margin:0; font-size: 13px; line-height: 20px;">
+                  Nos escribiste esto: <?=$args['message']?>
+                </p>
+              </td>
             </tr>
             <tr>
-                <td style="padding:10px 30px 50px 30px;background-color:#ffffff;">
-                    <p style="margin:0; text-align: center;">
-                        <a href="https://max.zixfox.com.ar"
-                            style="background: #b60307; text-decoration: none; padding: 13px 35px; color: #ffffff; border-radius: 99px; display:inline-block; mso-padding-alt:0;text-underline-color:#b60307; font-size: 17px;">
-                            <span style="mso-text-raise:10pt;font-weight:bold;">Visitar pagina</span>
-                        </a>
-                    </p>
-                </td>
+              <td style="padding:10px 30px 50px 30px;background-color:#ffffff;">
+                <p style="margin:0; text-align: center;">
+                  <a href="https://max.zixfox.com.ar"
+                      style="background: #b60307; text-decoration: none; padding: 13px 35px; color: #ffffff; border-radius: 99px; display:inline-block; mso-padding-alt:0;text-underline-color:#b60307; font-size: 17px;">
+                      <span style="mso-text-raise:10pt;font-weight:bold;">Visitar pagina</span>
+                  </a>
+                </p>
+              </td>
             </tr>
             <tr>
-                <td style="padding:30px;text-align:center;font-size:12px;background-color:#404040;color:#cccccc;">
-                    <p style="margin:0;font-size:14px;line-height:20px;">
-                        MAX &reg;, Salta, Argentina 2024
-                        <br>
-                        <a class="unsub" href="/"
-                            style="color:#cccccc;text-decoration:underline;">Desuscribirse</a>
-                    </p>
-                </td>
+              <td style="padding:30px;text-align:center;font-size:12px;background-color:#404040;color:#cccccc;">
+                <p style="margin:0;font-size:14px;line-height:20px;">
+                  MAX &reg;, Salta, Argentina 2024
+                  <br>
+                  <a class="unsub" href="/"
+                      style="color:#cccccc;text-decoration:underline;">Desuscribirse</a>
+                </p>
+              </td>
             </tr>
           </table>
         </td>
